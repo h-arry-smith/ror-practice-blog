f295bb842a43fd62ad66ad6a3f9a4fe27d4975e7
Harry Smith <harry@hsmith.net>
2022-08-08 15:23:55 +0100

    Edit Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   layout 'admin', only: %i[new edit]
 
-  before_action :get_article, only: %i[show edit]
+  before_action :get_article, only: %i[show edit update]
   def index
     @articles = Article.order(created_at: :desc).all
   end
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article.slug)
+      redirect_to article_path(@article.slug), notice: "Article created!"
     else
       render 'new', status: :unprocessable_entity, layout: 'admin'
     end
@@ -26,6 +26,11 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to article_path(@article.slug), notice: "Article saved!"
+    else
+      render 'edit', status: :unprocessable_entity, layout: 'admin'
+    end
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   validates :title, :body, :slug, presence: true
   validates :title, :body, :slug, length: { minimum: 2 }
   validates :slug, uniqueness: true
+
+  def to_param
+    slug
+  end
 end
